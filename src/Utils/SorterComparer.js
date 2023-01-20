a81306e5ba6d5de0f0193bed0b9a7b122a59bc82
杨乐达 <yangleda@outlook.com>
2023-01-20 10:16:20 +0800

    Fix isNaN

--- src/Utils/SorterComparer.js
+++ src/Utils/SorterComparer.js
@@ -27,11 +27,11 @@ function compareStatus(a, b) {
     return -1;
   }
 
-  if(!getTimeInSecs(a.status).isNaN()) {
+  if(!isNaN(getTimeInSecs(a.status))) {
     console.log("compareStatus error: getTimeInSecs returned NaN from" + a.status);
     return -1;
   }
-  if(!getTimeInSecs(b.status).isNaN()) {
+  if(!isNaN(getTimeInSecs(b.status))) {
     console.log("compareStatus error: getTimeInSecs returned NaN from" + b.status);
     return -1;
   }
